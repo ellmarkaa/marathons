4e0f44b9f69a3e9dee03834d4077ed839bed6612
Elmar Kochkonbaev <elmar.knv@gmail.com>
2024-12-27 18:33:07 +0600

    configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false },
-  modules: ['@nuxtjs/i18n', '@nuxt/eslint'],
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/i18n', '@nuxt/eslint', 'vue3-carousel-nuxt', '@nuxt/fonts'],
+  components: [
+    {
+      path: '~/components/ui',
+      pathPrefix: false, // Отключаем префиксы для компонентов в этой папке
+    },
+    '~/components',
+  ],
+
   i18n: {
     vueI18n: './i18n.config.ts',
   },
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
+  compatibilityDate: '2024-12-19',
 });
